582688bb8686c92d11bf05740812cc27f9b539b7
OmgbaKevin <kevinomgba1@gmail.com>
2025-10-15 10:58:18 +0100

    added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
 
   return (
     <>
       <h1 className='text-3xl font-bold underline'>hello world</h1>
+      <Button>Button</Button>
     </>
   )
 }
